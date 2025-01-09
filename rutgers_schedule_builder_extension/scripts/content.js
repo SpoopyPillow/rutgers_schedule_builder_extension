7e1734b49e76b6f7790e0fc33cd6c5d5aefb18a0
Michael Hu <mbh326684@gmail.com>
2025-01-09 14:47:57 -0500

    moved extraction into schedule class

--- rutgers_schedule_builder_extension/scripts/content.js
+++ rutgers_schedule_builder_extension/scripts/content.js
@@ -2,12 +2,12 @@ function restructure_original() {
     const tab_link = document.querySelector('div[widgetid="dijit_layout__TabButton_2"]');
     tab_link.addEventListener("click", update_schedule_builder);
 
+    schedule_data.extract_selected_courses();
     remove_original_meetings();
-    extract_selected_courses();
 }
 
 function update_schedule_builder() {
-    extract_selected_courses();
+    schedule_data.extract_selected_courses();
     remove_original_meetings();
 }
 
@@ -17,52 +17,6 @@ function remove_original_meetings() {
     for (const meeting of meetings) {
         remove_element(meeting);
     }
-}
-
-function extract_selected_courses() {
-    schedule_data = new Schedule();
-
-    const section_select = document.getElementById("SectionSelectID");
-
-    const courses = section_select.querySelectorAll(".course");
-    for (const course of courses) {
-        const course_data = new Course(
-            course.querySelector(".number .unit-code").textContent +
-                ":" +
-                course.querySelector(".number .subject").textContent +
-                ":" +
-                course.querySelector(".number .number").textContent,
-            course.querySelector(".title").textContent,
-        );
-
-        const sections = course.querySelectorAll('tr[dojoattachpoint="sectionMainArea"]');
-        for (const section of sections) {
-            const section_data = new Section(
-                section.querySelector('td[title="Index Number"]').textContent,
-                section.querySelector('td[title="Section Number"]').textContent,
-                section.querySelector('td[title="Section Status"]').textContent,
-            );
-
-            const meetings = section.querySelectorAll('tr[id^="csp_view_domain_Meeting_"]');
-            for (const meeting of meetings) {
-                const meeting_data = new Meeting(
-                    meeting.querySelector(".weekday").textContent.toLowerCase(),
-                    format_time(meeting.querySelector(".time").textContent.split("-")[0]),
-                    format_time(meeting.querySelector(".time").textContent.split("-")[1]),
-                    meeting.querySelector(".location3").textContent.toLowerCase(),
-                    meeting.querySelector(".location").textContent
-                );
-
-                section_data.append_meeting(meeting_data);
-            }
-
-            course_data.append_section(section_data, section.querySelector('input').checked);
-        }
-
-        schedule_data.append_course(course_data);
-    }
-
-    console.log(schedule_data);
 }
 
 async function inject_content() {
--- rutgers_schedule_builder_extension/scripts/schedule.js
+++ rutgers_schedule_builder_extension/scripts/schedule.js
@@ -4,9 +4,55 @@ class Schedule {
         this.section_index = new Array();
     }
 
-    append_course(course, selected=-1) {
+    append_course(course, selected = -1) {
         this.courses.push(course);
         this.section_index.push(selected);
+    }
+
+    extract_selected_courses() {
+        schedule_data = new Schedule();
+
+        const section_select = document.getElementById("SectionSelectID");
+
+        const courses = section_select.querySelectorAll(".course");
+        for (const course of courses) {
+            const course_data = new Course(
+                course.querySelector(".number .unit-code").textContent +
+                    ":" +
+                    course.querySelector(".number .subject").textContent +
+                    ":" +
+                    course.querySelector(".number .number").textContent,
+                course.querySelector(".title").textContent
+            );
+
+            const sections = course.querySelectorAll('tr[dojoattachpoint="sectionMainArea"]');
+            for (const section of sections) {
+                const section_data = new Section(
+                    section.querySelector('td[title="Index Number"]').textContent,
+                    section.querySelector('td[title="Section Number"]').textContent,
+                    section.querySelector('td[title="Section Status"]').textContent
+                );
+
+                const meetings = section.querySelectorAll('tr[id^="csp_view_domain_Meeting_"]');
+                for (const meeting of meetings) {
+                    const meeting_data = new Meeting(
+                        meeting.querySelector(".weekday").textContent.toLowerCase(),
+                        format_time(meeting.querySelector(".time").textContent.split("-")[0]),
+                        format_time(meeting.querySelector(".time").textContent.split("-")[1]),
+                        meeting.querySelector(".location3").textContent.toLowerCase(),
+                        meeting.querySelector(".location").textContent
+                    );
+
+                    section_data.append_meeting(meeting_data);
+                }
+
+                course_data.append_section(section_data, section.querySelector("input").checked);
+            }
+
+            schedule_data.append_course(course_data);
+        }
+
+        console.log(schedule_data);
     }
 }
 
@@ -18,7 +64,7 @@ class Course {
         this.selected = new Array();
     }
 
-    append_section(section, selected=true) {
+    append_section(section, selected = true) {
         this.sections.push(section);
         this.selected.push(selected);
     }
