83ca8f0779ceb70af61234c703a5e8d48a85d44c
Michael Hu <mbh326684@gmail.com>
2025-01-09 16:27:53 -0500

    created sidebar panel

--- rutgers_schedule_builder_extension/scripts/content.js
+++ rutgers_schedule_builder_extension/scripts/content.js
@@ -2,13 +2,33 @@ function restructure_original() {
     const tab_link = document.querySelector('div[widgetid="dijit_layout__TabButton_2"]');
     tab_link.addEventListener("click", update_schedule_builder);
 
-    schedule_data.extract_selected_courses();
-    remove_original_meetings();
+    structure_sidebar();
+    update_schedule_builder();
+}
+
+function structure_sidebar() {
+    const schedule_builder = document.getElementById("CSPBuildScheduleTab");
+
+    // SCHEDULE VIEW
+    const schedule_view = document.createElement("div");
+    schedule_view.className =
+        "schedule_view dijitContentPane dijitBorderContainer-dijitContentPane dijitBorderContainerPane";
+    schedule_view.style = "inset: 5px 5px 5px 310px";
+    [...schedule_builder.childNodes].forEach((child) => schedule_view.appendChild(child));
+    schedule_builder.appendChild(schedule_view);
+
+    // SCHEDULE SIDEBAR
+    const sidebar = document.createElement("div");
+    sidebar.className =
+        "schedule_sidebar box dijitContentPane dijitBorderContainer-dijitContentPane dijitBorderContainerPane";
+    sidebar.style = "top: 5px; bottom: 5px; left: 5px";
+
+    schedule_builder.prepend(sidebar);
 }
 
 function update_schedule_builder() {
-    schedule_data.extract_selected_courses();
     remove_original_meetings();
+    schedule_data.extract_selected_courses();
 }
 
 function remove_original_meetings() {
