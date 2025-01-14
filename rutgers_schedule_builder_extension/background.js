96676d9d176be13e474433b11f271e8e4587ffd7
Michael Hu <mbh326684@gmail.com>
2025-01-13 23:41:38 -0500

    removed popup and direct to csp when clicking extension:

--- rutgers_schedule_builder_extension/background.js
+++ rutgers_schedule_builder_extension/background.js
@@ -1,3 +1,8 @@
 chrome.runtime.onInstalled.addListener(function () {
     console.log("Extension installed");
 });
+
+chrome.action.onClicked.addListener(function (activeTab) {
+    var url = "https://sims.rutgers.edu/csp/";
+    chrome.tabs.create({ url: url });
+});
